cf2ba3886d113cfd45a2fc097f6abd90517fe64b
aiml45234 <159514759+aiml45234@users.noreply.github.com>
2024-07-18 17:57:27 +0530

    Update chatapp.py

--- chatapp.py
+++ chatapp.py
@@ -23,6 +23,7 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 def get_pdf_text(pdf_docs):
     text=""
     for pdf in pdf_docs:
+        print(pdf)
         pdf_reader= PdfReader(pdf)
         for page in pdf_reader.pages:
             text+= page.extract_text()
